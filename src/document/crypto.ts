3d85dbdc92406c48164ade8d75aae4faf7ecc506
jesus <jesus@heavens.com>
2021-03-24 17:57:56 +0200

    test fix

--- src/document/crypto.ts
+++ src/document/crypto.ts
@@ -1,38 +1,38 @@
 /* eslint-disable no-bitwise */
-import Crypto from "crypto-es";
+import CryptoES from "crypto-es";
 import { bytesToInt32Array, hexStringToBytes, int32ArrayToBytes } from "./byte-functions";
 
-export function toWordArray(data: Uint8Array): Crypto.lib.WordArray {
+export function toWordArray(data: Uint8Array): CryptoES.lib.WordArray {
   // return Crypto.lib.WordArray.create(Array.from(bytesToInt32Array(data)));
-  return Crypto.lib.WordArray.create(data);
+  return CryptoES.lib.WordArray.create(data);
 }
 
-export function wordArrayToBytes(wordArray: Crypto.lib.WordArray): Uint8Array {
+export function wordArrayToBytes(wordArray: CryptoES.lib.WordArray): Uint8Array {
   return int32ArrayToBytes(wordArray.words).slice(0, wordArray.sigBytes);
 }
 
-export function md5(data: Uint8Array | Crypto.lib.WordArray): Crypto.lib.WordArray {
+export function md5(data: Uint8Array | CryptoES.lib.WordArray): CryptoES.lib.WordArray {
   if (data instanceof Uint8Array) {
     data = toWordArray(data);
   }    
-  const result = Crypto.MD5(data);
+  const result = CryptoES.MD5(data);
   return result;
 }
 
-export function rc4(data: Uint8Array | Crypto.lib.WordArray, 
-  key: Uint8Array | Crypto.lib.WordArray): Crypto.lib.WordArray {
+export function rc4(data: Uint8Array | CryptoES.lib.WordArray, 
+  key: Uint8Array | CryptoES.lib.WordArray): CryptoES.lib.WordArray {
   if (data instanceof Uint8Array) {
     data = toWordArray(data);
   }
   if (key instanceof Uint8Array) {
     key = toWordArray(key);
   }   
-  const result = Crypto.RC4.encrypt(data, key).ciphertext;
+  const result = CryptoES.RC4.encrypt(data, key).ciphertext;
   return result;
 }
 
-export function aes(data: Uint8Array | Crypto.lib.WordArray, 
-  key: Uint8Array | Crypto.lib.WordArray, decrypt = false): Crypto.lib.WordArray {
+export function aes(data: Uint8Array | CryptoES.lib.WordArray, 
+  key: Uint8Array | CryptoES.lib.WordArray, decrypt = false): CryptoES.lib.WordArray {
   if (data instanceof Uint8Array) {
     data = toWordArray(data);
   }
@@ -41,7 +41,7 @@ export function aes(data: Uint8Array | Crypto.lib.WordArray,
   }    
 
   if (decrypt) {
-    const ivWordArray = Crypto.lib.WordArray.create(data.words.slice(0, 4));
+    const ivWordArray = CryptoES.lib.WordArray.create(data.words.slice(0, 4));
 
     // const result = Crypto.AES.decrypt(data.toString(Crypto.enc.Base64), key, {
     //   mode: Crypto.mode.CBC, 
@@ -49,16 +49,16 @@ export function aes(data: Uint8Array | Crypto.lib.WordArray,
     //   padding: Crypto.pad.Pkcs7,
     // });
 
-    const d = Crypto.algo.AES.createDecryptor(key, {
-      mode: Crypto.mode.CBC, 
+    const d = CryptoES.algo.AES.createDecryptor(key, {
+      mode: CryptoES.mode.CBC, 
       iv: ivWordArray, 
-      padding: Crypto.pad.Pkcs7,
+      padding: CryptoES.pad.Pkcs7,
     });
     const result = d.finalize(data);
 
     return result;
   } else {
-    const ivWordArray = Crypto.lib.WordArray.random(16);
+    const ivWordArray = CryptoES.lib.WordArray.random(16);
 
     // const result = Crypto.AES.encrypt(data, key, {
     //   mode: Crypto.mode.CBC, 
@@ -66,10 +66,10 @@ export function aes(data: Uint8Array | Crypto.lib.WordArray,
     //   padding: Crypto.pad.Pkcs7,
     // }).ciphertext;
 
-    const e = Crypto.algo.AES.createEncryptor(key, {
-      mode: Crypto.mode.CBC, 
+    const e = CryptoES.algo.AES.createEncryptor(key, {
+      mode: CryptoES.mode.CBC, 
       iv: ivWordArray, 
-      padding: Crypto.pad.Pkcs7,
+      padding: CryptoES.pad.Pkcs7,
     });
     const result = e.finalize(data);
 
